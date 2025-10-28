d9d8d92f576b28744e66bce303d8149151658347
movadmin-dotcom <movaastrology@gmail.com>
2025-10-28 16:06:21 -0400

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer(){
-  return (
-    <footer className="py-8 text-center text-sm text-gray-600">
-      © {new Date().getFullYear()} Your Astro — All rights reserved.
-    </footer>
-  )
-}
