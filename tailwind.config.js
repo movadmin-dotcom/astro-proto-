8e408c2fdf627b87d9f4e7e91e57c7f2fee1534b
movadmin-dotcom <movaastrology@gmail.com>
2025-10-28 11:26:25 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cosmic: "#0b2140",
+        accent: "#d4b563"
+      }
+    }
+  },
+  plugins: []
+};
