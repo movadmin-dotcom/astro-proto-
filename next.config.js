9395c64b005d544dd30302fd06d0d0a57dc35141
movadmin-dotcom <movaastrology@gmail.com>
2025-10-28 11:24:56 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [], // add any external image domains here
+  },
+};
 
+module.exports = nextConfig;
