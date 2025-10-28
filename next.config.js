8ab2221c812da3fe241396031a5457c1428924be
movadmin-dotcom <movaastrology@gmail.com>
2025-10-28 16:11:20 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [], // add any external image domains here
-  },
-};
-
-module.exports = nextConfig;
